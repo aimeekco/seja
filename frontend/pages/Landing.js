44e8585b533cbdf5c37aa8407d5eea091b78db14
Aimee Co <aimeekco@gmail.com>
2024-04-13 15:57:13 -0700

    try again

--- frontend/pages/Landing.js
+++ frontend/pages/Landing.js
@@ -18,9 +18,6 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function LoginForm() {
-
-
 
 function LoginForm() {
   const handleSubmit = async (event) => {
