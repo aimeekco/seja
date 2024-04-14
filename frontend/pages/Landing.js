0238018ae370f5a4812fbb1cb0c866d2986de822
JackChin3 <Jackspencerchin@gmail.com>
2024-04-14 01:38:17 -0700

    bruh

--- frontend/pages/Landing.js
+++ frontend/pages/Landing.js
@@ -28,7 +28,7 @@ function LoginForm() {
   
     const response = await fetch('http://127.0.0.1:5000/login', {
       method: 'POST',
-      //credentials: 'include',
+      credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
       },
