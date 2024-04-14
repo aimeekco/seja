529fb6e34c8b99ed613b5794236d807d97babee6
JackChin3 <Jackspencerchin@gmail.com>
2024-04-13 20:15:26 -0700

    backend stuff

--- frontend/pages/CreateAccount.js
+++ frontend/pages/CreateAccount.js
@@ -1,8 +1,14 @@
 'use client';
 import React, { useState } from 'react';
 import '../styles/createaccount.css'
+import { useRouter } from 'next/navigation';
+
 
 const LoginComponent = () => {
+
+  const router = useRouter();
+
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -10,20 +16,49 @@ const LoginComponent = () => {
     setEmail(event.target.value);
   };
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
+    const response = await fetch('http://127.0.0.1:5000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    
     // Handle the login logic here
-    console.log(`Email: ${email}, Password: ${password}`);
+    
+    router.push('/UserInfo')
+    //console.log(`Email: ${email}, Password: ${password}`);
   };
 
   return (
     <div className="login-container">
       <h2 className="login-title">Create Account</h2>
       <form onSubmit={handleLogin} className="login-form">
+      <label htmlFor="name" className="login-label">Name</label>
+        <input
+          type="name"
+          id="name"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={handleNameChange}
+          className="login-input"
+        />
         <label htmlFor="email" className="login-label">Email</label>
         <input
           type="email"
@@ -44,7 +79,7 @@ const LoginComponent = () => {
           onChange={handlePasswordChange}
           className="login-input"
         />
-        <button type="submit" className="login-button">Login</button>
+        <button type="submit" className="login-button">Create Account</button>
       </form>
     </div>
   );
