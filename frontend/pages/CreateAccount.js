80f3b9dd3dba3c689ceaa5b2ddb23b087c3659da
Aimee Co <aimeekco@gmail.com>
2024-04-13 21:21:51 -0700

    navbar

--- frontend/pages/CreateAccount.js
+++ frontend/pages/CreateAccount.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import '../styles/createaccount.css'
+import Navbar from './Navbar';
 
 const LoginComponent = () => {
   const [email, setEmail] = useState('');
@@ -21,31 +22,34 @@ const LoginComponent = () => {
   };
 
   return (
-    <div className="login-container">
-      <h2 className="login-title">Create Account</h2>
-      <form onSubmit={handleLogin} className="login-form">
-        <label htmlFor="email" className="login-label">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleEmailChange}
-          className="login-input"
-        />
-        <label htmlFor="password" className="login-label">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-          className="login-input"
-        />
-        <button type="submit" className="login-button">Login</button>
-      </form>
+    <div>
+      <Navbar />
+      <div className="login-container">
+        <h2 className="login-title">Create Account</h2>
+        <form onSubmit={handleLogin} className="login-form">
+          <label htmlFor="email" className="login-label">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            className="login-input"
+          />
+          <label htmlFor="password" className="login-label">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="login-input"
+          />
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
